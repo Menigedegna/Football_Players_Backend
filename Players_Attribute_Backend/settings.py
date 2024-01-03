61ae4b8d176aca865d21d7eb0a75d57736ed1092
Mariamawit <mariamawit.ashenafi2011@gmail.com>
2024-01-03 18:58:32 +0000

    fix: updated settings

--- Players_Attribute_Backend/settings.py
+++ Players_Attribute_Backend/settings.py
@@ -2,6 +2,7 @@
 Django settings for Players_Attribute_Backend project.
 
 """
+import os
 
 from pathlib import Path
 
@@ -13,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a4b7tjw(3-r!u7edmq*^pv*o$+k8+2+c^)62v@$jh8kj1lg44q'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
